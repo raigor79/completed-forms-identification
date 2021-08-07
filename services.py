6611c9e6da122e885fcf1b153f3782a337f0abfc
raigor <raigor@mail.ru>
2021-08-07 03:12:24 +0300

    create file servises

---
+++ services.py
@@ -0,0 +1,11 @@
+import typing
+
+from typing import Optional
+from pydantic import fields
+from pydantic.dataclasses import dataclass
+from pydantic import BaseModel, types
+from pydantic.fields import ModelField, Field
+
+
+if __name__ == "__main__":
+    pass
